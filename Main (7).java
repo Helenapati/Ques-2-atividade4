120b874ad38e08ab99d052563841860a917c2a8a
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-11-17 22:44:52 -0300

    Aluna: Helena Patrícia de Souza Silva
    Disciplina: Linguagem de Programação I
    Data: 17/11/2021

---
+++ Main (7).java
@@ -0,0 +1,38 @@
+//questão 2//
+
+  public class{
+      
+      HashSet<tênis> dset = new HashSet<tênis>();
+      dset.add (new tênis(1));
+      dset.add (new tênis(2));
+      dset.add (new tênis(3));
+      dset.add (new tênis(4));
+      dset.add (new tênis(5));
+      dset.add (new tênis(6));
+      dset.add (new tênis(7));
+      dset.add (new tênis(8));
+      dset.add (new tênis(9));
+      dset.add (new tênis(10));
+      
+      System.out.println(tênis(1);
+      System.out.println(tênis(2);
+      System.out.println(tênis(3);
+      System.out.println(tênis(4);
+      System.out.println(tênis(5);
+      System.out.println(tênis(6);
+      System.out.println(tênis(7);
+      System.out.println(tênis(8);
+      System.out.println(tênis(9);
+      System.out.println(tênis(10);
+  }
+    
+
+            
+            
+        
+            
+            
+            
+            
+            
+    
